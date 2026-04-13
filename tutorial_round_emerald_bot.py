0a3e6d197b9900f52b370df7a4ab19fa9deb8c28
Adrian Zaremba <adrian.zaremba8@gmail.com>
2026-04-13 14:13:07 -0400

    Overfitting emerald strategy draft #1

---
+++ tutorial_round_emerald_bot.py
@@ -0,0 +1,32 @@
+from datamodel import OrderDepth, TradingState, Order
+from typing import List
+
+class Trader:
+    def run(self, state: TradingState):
+        result = {}
+
+        product = "EMERALDS"
+        BUY_PRICE = 9993
+        SELL_PRICE = 10007
+        LIMIT = 80
+
+        orders: List[Order] = []
+
+        pos = state.position.get(product, 0)
+
+        # how much we are allowed to still buy/sell
+        buy_qty = LIMIT - pos          # if pos=50, can buy 30 more
+        sell_qty = LIMIT + pos         # if pos=-20, can sell 60 more
+
+        if buy_qty > 0:
+            orders.append(Order(product, BUY_PRICE, buy_qty))
+
+        if sell_qty > 0:
+            orders.append(Order(product, SELL_PRICE, -sell_qty))
+
+        result[product] = orders
+
+        traderData = state.traderData  # or whatever you want to store
+        conversions = 0                # no conversions needed
+
+        return result, conversions, traderData
